e7b9c4cdaba21fa0b66fb4b168603a834427b1ae
self_designer <ezhiloli07@gmail.com>
2022-10-07 20:08:12 +0530

    Basic Middleware has been Added Features Pending

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import {createStore} from 'redux';
+import {createStore,applyMiddleware} from 'redux';
 import rootReducer  from './reducers'
  
+
+const logger = function({dispatch,getState}){
+
+    return function(next){
+        return function(action){
+            console.log('ACTION_TYPE',action.type);
+            next(action);
+        }
+    }
+
+}
+
 // passing the Reducer Here
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,applyMiddleware(logger));
 // Store Movie
 console.log('Redux Store',store);
 // console.log('Redux State',store.getState());
